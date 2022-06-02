bb32b3191ece0dfb97a30b7977065e27df140b06
SmileBoy59 <105280187+SmileBoy59@users.noreply.github.com>
2022-06-02 18:05:33 +0900

    df

--- example.py
+++ example.py
@@ -1,8 +1,23 @@
 import numpy as np
+from scipy.optimize import linear_sum_assignment
+import random
 
 n=5
-import random
 
 ar = np.random.randint(10, size=(n,n))
 array = ar+1
 print(array)
+
+
+
+a,b = linear_sum_assignment(array)
+print(array[a,b].sum())
+
+"""
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            for l in range(n):
+                if i!=j and j!=k and k!=l and i!=k and i!=l and j!=l:
+                    #sol = 0
+"""
